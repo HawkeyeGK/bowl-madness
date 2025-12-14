e4986bdf17907515cfdcf3c8b6ae70004af70f77
Jason Nash <JasonRNash@gmail.com>
2025-12-14 11:00:39 -0600

    Fix: Add Newtonsoft JsonProperty to UserProfile for Cosmos SDK compatibility

--- BowlPoolManager.Core/Domain/UserProfile.cs
+++ BowlPoolManager.Core/Domain/UserProfile.cs
@@ -1,9 +1,11 @@
 using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace BowlPoolManager.Core.Domain
 {
     public class UserProfile
     {
+        [JsonProperty("id")] // <--- Critical for Cosmos SDK
         [JsonPropertyName("id")]
         public string Id { get; set; } = string.Empty; // This will be the SWA User ID
 
@@ -14,9 +16,11 @@ namespace BowlPoolManager.Core.Domain
         public string DisplayName { get; set; } = string.Empty;
 
         // Roles: "SuperAdmin", "Admin", "Player"
+        [JsonProperty("appRole")]
         [JsonPropertyName("appRole")]
         public string AppRole { get; set; } = "Player";
 
+        [JsonProperty("type")]
         [JsonPropertyName("type")]
         public string Type { get; set; } = "UserProfile";
         
